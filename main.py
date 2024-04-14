bbae0cbf977e380fbef481949fe7752459555bba
Valera <valerylarionov06@gmail.com>
2024-04-14 16:35:13 +0300

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pyautogui
 import time
 
+# при запуске должна быть выбрана английская раскладка
 time.sleep(3)  # время, чтобы перейти в чат
 while True:
     pyautogui.typewrite('@everyone')
     pyautogui.press("enter")
+
+    # чтобы настроить частоту отправки:
+    # pyautogui.press("enter", interval=0.2) interval - частота отправки - сейчас каждые 0.2 секунды
